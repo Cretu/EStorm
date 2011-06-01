74294b76e1064a68fbeaecd648d89ed10730bfdb
Cretu <errooo@gmail.com>
2011-06-02 00:03:11 +0800

    初始化项目 Signed-off-by: Cretu <errooo@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EStorm::Application.config.secret_token = 'f1c001627889057a3100207a6b95156bbfc79b802a9d0ce0aa4052bae520167d86d39b05b4e96a5e72f70f51e43d4928a38d8668246903d8d01f77f4070eec00'
